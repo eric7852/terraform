a4eec882a15831bdb91807eeee181b57baadd6c9
Seungjun <vdyxz1357@naver.com>
2021-10-27 17:14:20 +0900

    rtas_step6

---
+++ 06_rtas.tf
@@ -0,0 +1,9 @@
+resource "aws_route_table_association" "seungjun_rtas_a" {
+  subnet_id      = aws_subnet.seungjun_puba.id
+  route_table_id = aws_route_table.seungjun_rt.id
+}
+
+resource "aws_route_table_association" "seungjun_rtas_c" {
+  subnet_id      = aws_subnet.seungjun_pubc.id
+  route_table_id = aws_route_table.seungjun_rt.id
+}
